6a2c6b19a3b3cdab9436bf73d57a1f2116d4edd8
Leonardo Kenta <167496247+Leokenta@users.noreply.github.com>
2024-10-19 17:15:41 -0300

    Create main.js

---
+++ main.js
@@ -0,0 +1,29 @@
+require("dotenv").config();
+
+const express = require("express");
+const mysql = require("mysql2");
+const app = express();
+
+// Criação de um pool de conexões
+const pool = mysql.createPool({
+  host: process.env.host,
+  user: process.env.user,
+  password: process.env.password,
+  database: process.env.database,
+});
+
+// Endpoint para buscar clientes
+app.get('/', (req, res) => {
+  pool.query("SELECT * FROM cliente", (err, results) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).send("Erro ao consultar o banco de dados.");
+    }
+    res.json(results);
+  });
+});
+
+// Inicia o servidor
+app.listen(3000, () => {
+  console.log("Server is running on port 3000");
+});
